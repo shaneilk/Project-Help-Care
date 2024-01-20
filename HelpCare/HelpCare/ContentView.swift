eb7ca7fc9732d9ed17981768a8afa62257ebcf28
Joseph Nevado <josephnevado12@gmail.com>
2024-01-20 14:34:03 -0800

    add Xcode files

---
+++ HelpCare/HelpCare/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelpCare
+//
+//  Created by Joseph Nevado on 2024-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelpCare/HelpCare/HelpCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpCareApp.swift
+//  HelpCare
+//
+//  Created by Joseph Nevado on 2024-01-20.
+//
+
+import SwiftUI
+
+@main
+struct HelpCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
